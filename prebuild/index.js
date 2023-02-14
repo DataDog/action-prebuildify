6cf2342a499c17c4257c93aa1412e9284ac27f4b
David Lee <thedavl2001@gmail.com>
2023-02-14 13:22:44 -0800

    Add a feature to build for a NAPI-RS project (#22)

--- prebuild/index.js
+++ prebuild/index.js
@@ -15,9 +15,11 @@ const shell = process.env.SHELL
 
 const {
   NAPI = 'false',
+  NAPI_RS = 'false',
   NODE_VERSIONS = '>=12',
   POSTBUILD = '',
   PREBUILD = '',
+  DIRECTORY_PATH = '.',
   TARGET_NAME = 'addon'
 } = process.env
 
@@ -33,6 +35,17 @@ const targets = [
   { version: '19.0.0', abi: '111' }
 ].filter(target => semver.satisfies(target.version, NODE_VERSIONS))
 
+const napiTargets = {
+  'linux-musl': 'x86_64-unknown-linux-musl',
+  'linux-arm64': 'aarch64-unknown-linux-gnu',
+  'linux-arm': 'armv7-unknown-linux-gnueabihf',
+  'linux-x64': 'x86_64-unknown-linux-gnu',
+  'darwin-arm64': 'aarch64-apple-darwin',
+  'darwin-x64': 'x86_64-apple-darwin',
+  'win32-ia32': 'i686-pc-windows-msvc',
+  'win32-x64': 'x86_64-pc-windows-msvc'
+}
+
 prebuildify()
 
 function prebuildify () {
@@ -45,6 +58,8 @@ function prebuildify () {
 
   if (NAPI === 'true') {
     prebuildTarget(arch, { version: targets[0].version, abi: 'napi' })
+  } else if (NAPI_RS === 'true') {
+    prebuildTarget(arch, { version: targets[0].version })
   } else {
     targets.forEach(target => prebuildTarget(arch, target))
   }
@@ -55,25 +70,61 @@ function prebuildify () {
 }
 
 function prebuildTarget (arch, target) {
+  if (NAPI_RS === 'true' && platform === 'linux' && arch === 'ia32') return
+
   if (platform === 'linux' && arch === 'ia32' && semver.gte(target.version, '14.0.0')) return
   if (platform === 'win32' && arch === 'ia32' && semver.gte(target.version, '18.0.0')) return
 
-  const output = `prebuilds/${platform}${libc}-${arch}/node-${target.abi}.node`
-  const cmd = [
-    'node-gyp rebuild',
-    `--target=${target.version}`,
-    `--target_arch=${arch}`,
-    `--arch=${arch}`,
-    `--devdir=${cache}`,
-    '--release',
-    '--jobs=max',
-    '--build_v8_with_gn=false',
-    '--v8_enable_pointer_compression=""',
-    '--v8_enable_31bit_smis_on_64bit_arch=""',
-    '--enable_lto=false'
-  ].join(' ')
+  let cmd
+
+  if (NAPI_RS === 'true') {
+    const buildTarget = napiTargets[`${platform}-${arch}`]
+    const rustEnvFlags = (platform === 'linux' && libc === 'musl')
+      ? 'RUSTFLAGS="-C target-feature=-crt-static"'
+      : ''
+
+    execSync("curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y", { stdio, shell })
+    process.env.PATH += path.delimiter + process.env.HOME + path.sep + '.cargo' + path.sep + 'bin'
+
+    let napiBuildCommand;
+
+    if (platform === 'win32') {
+      napiBuildCommand = `npx ${__dirname + path.sep}node_modules${path.sep}@napi-rs${path.sep}cli build`
+    } else {
+      napiBuildCommand = `${__dirname + path.sep}node_modules${path.sep}.bin${path.sep}napi build`
+    }
+
+    cmd = [
+      `rustup target add ${buildTarget} &&`,
+      `cd ${DIRECTORY_PATH} &&`,
+      `${rustEnvFlags}`,
+      napiBuildCommand,
+      '--release',
+      `--target=${buildTarget}`
+    ].join(' ')
+  } else {
+    cmd = [
+      'node-gyp rebuild',
+      `--target=${target.version}`,
+      `--target_arch=${arch}`,
+      `--arch=${arch}`,
+      `--devdir=${cache}`,
+      '--release',
+      '--jobs=max',
+      '--build_v8_with_gn=false',
+      '--v8_enable_pointer_compression=""',
+      '--v8_enable_31bit_smis_on_64bit_arch=""',
+      '--enable_lto=false'
+    ].join(' ')
+  }
 
   execSync(cmd, { stdio, shell })
 
-  fs.copyFileSync(`build/Release/${TARGET_NAME}.node`, output)
+  if (NAPI_RS === 'true') {
+    const output = `prebuilds/${platform}${libc}-${arch}/${TARGET_NAME}.node`
+    fs.copyFileSync(`${DIRECTORY_PATH}/${TARGET_NAME}.node`, output)
+  } else {
+    const output = `prebuilds/${platform}${libc}-${arch}/node-${target.abi}.node`
+    fs.copyFileSync(`${DIRECTORY_PATH}/build/Release/${TARGET_NAME}.node`, output)
+  }
 }
