e98b218f0bd601d366308017a03354ab05e29492
rochdev <roch.devost@gmail.com>
2024-02-13 13:15:58 -0500

    add support for building neon binaries

--- prebuild/index.js
+++ prebuild/index.js
@@ -16,22 +16,27 @@ const shell = process.env.SHELL
 const {
   NAPI = 'false',
   NAPI_RS = 'false',
+  NEON = 'false',
   NODE_VERSIONS = '>=12',
   POSTBUILD = '',
   PREBUILD = '',
   DIRECTORY_PATH = '.',
   TARGET_NAME = 'addon',
-  NODE_HEADERS_DIRECTORY = path.join(os.tmpdir(), 'prebuilds'),
+  NODE_HEADERS_DIRECTORY = path.join(os.tmpdir(), 'prebuilds')
 } = process.env
 
 // https://nodejs.org/en/download/releases/
 const targets = getFilteredNodeTargets(NODE_VERSIONS)
 
 const napiTargets = {
-  'linux-musl': 'x86_64-unknown-linux-musl',
   'linux-arm64': 'aarch64-unknown-linux-gnu',
   'linux-arm': 'armv7-unknown-linux-gnueabihf',
   'linux-x64': 'x86_64-unknown-linux-gnu',
+  'linuxglibc-arm64': 'aarch64-unknown-linux-gnu',
+  'linuxglibc-arm': 'armv7-unknown-linux-gnueabihf',
+  'linuxglibc-x64': 'x86_64-unknown-linux-gnu',
+  'linuxmusl-x64': 'x86_64-unknown-linux-musl',
+  'linuxmusl-arm64': 'aarch64-unknown-linux-musl',
   'darwin-arm64': 'aarch64-apple-darwin',
   'darwin-x64': 'x86_64-apple-darwin',
   'win32-ia32': 'i686-pc-windows-msvc',
@@ -50,7 +55,7 @@ function prebuildify () {
 
   if (NAPI === 'true') {
     prebuildTarget(arch, { version: targets[0].version, abi: 'napi' })
-  } else if (NAPI_RS === 'true') {
+  } else if (NAPI_RS === 'true' || NEON === 'true') {
     prebuildTarget(arch, { version: targets[0].version })
   } else {
     targets.forEach(target => prebuildTarget(arch, target))
@@ -63,6 +68,7 @@ function prebuildify () {
 
 function prebuildTarget (arch, target) {
   if (NAPI_RS === 'true' && platform === 'linux' && arch === 'ia32') return
+  if (NEON === 'true' && platform === 'linux' && arch === 'ia32') return
 
   if (platform === 'linux' && arch === 'ia32' && semver.gte(target.version, '14.0.0')) return
   if (platform === 'win32' && arch === 'ia32' && semver.gte(target.version, '18.0.0')) return
@@ -70,15 +76,9 @@ function prebuildTarget (arch, target) {
   let cmd
 
   if (NAPI_RS === 'true') {
-    const buildTarget = napiTargets[`${platform}-${arch}`]
-    const rustEnvFlags = (platform === 'linux' && libc === 'musl')
-      ? 'RUSTFLAGS="-C target-feature=-crt-static"'
-      : ''
+    installRust()
 
-    execSync("curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y", { stdio, shell })
-    process.env.PATH += path.delimiter + process.env.HOME + path.sep + '.cargo' + path.sep + 'bin'
-
-    let napiBuildCommand;
+    let napiBuildCommand
 
     if (platform === 'win32') {
       napiBuildCommand = `npx ${__dirname + path.sep}node_modules${path.sep}@napi-rs${path.sep}cli build`
@@ -86,14 +86,11 @@ function prebuildTarget (arch, target) {
       napiBuildCommand = `${__dirname + path.sep}node_modules${path.sep}.bin${path.sep}napi build`
     }
 
-    cmd = [
-      `rustup target add ${buildTarget} &&`,
-      `cd ${DIRECTORY_PATH} &&`,
-      `${rustEnvFlags}`,
-      napiBuildCommand,
-      '--release',
-      `--target=${buildTarget}`
-    ].join(' ')
+    cmd = `cd ${DIRECTORY_PATH} && ${napiBuildCommand} --release`
+  } else if (NEON === 'true') {
+    installRust()
+
+    cmd = `cd ${DIRECTORY_PATH} && npm run build-release`
   } else {
     cmd = [
       'node-gyp rebuild',
@@ -117,8 +114,24 @@ function prebuildTarget (arch, target) {
   if (NAPI_RS === 'true') {
     const output = `prebuilds/${platform}${libc}-${arch}/${TARGET_NAME}.node`
     fs.copyFileSync(`${DIRECTORY_PATH}/${TARGET_NAME}.node`, output)
+  } else if (NEON === 'true') {
+    const output = `prebuilds/${platform}${libc}-${arch}/${TARGET_NAME}.node`
+    fs.copyFileSync(`${DIRECTORY_PATH}/index.node`, output)
   } else {
     const output = `prebuilds/${platform}${libc}-${arch}/node-${target.abi}.node`
     fs.copyFileSync(`${DIRECTORY_PATH}/build/Release/${TARGET_NAME}.node`, output)
   }
+}
+
+function installRust () {
+  execSync("curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y", { stdio, shell })
+
+  process.env.PATH += path.delimiter + process.env.HOME + path.sep + '.cargo' + path.sep + 'bin'
+  process.env.CARGO_BUILD_TARGET = napiTargets[`${platform}${libc}-${arch}`]
+
+  if (platform === 'linux' && libc === 'musl') {
+    process.env.RUSTFLAGS = '-C target-feature=-crt-static'
+  }
+
+  execSync('rustup toolchain install nightly && rustup component add rust-src --toolchain nightly')
 }
