be38827bb36e40e47af43999df11af8bf5caba2b
rochdev <roch.devost@gmail.com>
2024-03-11 17:24:34 -0400

    disable neon testing for unsupported node versions

--- compute-matrix/index.js
+++ compute-matrix/index.js
@@ -2,13 +2,10 @@
 
 const { parseArgs } = require('node:util')
 
-// Neon doesn't support Node 12
-const defaultMin = process.env.NEON === 'true' ? '14' : '12'
-
 const { values } = parseArgs({
   args: process.argv.slice(2),
   options: {
-    min: { type: 'string', default: defaultMin },
+    min: { type: 'string', default: '12' },
     max: { type: 'string', default: '21' },
     step: { type: 'string', default: '2' }
   }
--- test/neon/test.js
+++ test/neon/test.js
@@ -1,5 +1,8 @@
 'use strict'
 
-const { hello } = require('node-gyp-build')(__dirname)
+// Neon only supports Node >=14
+if (process.versions.node.split('.')[0] >= '14') {
+  const { hello } = require('node-gyp-build')(__dirname)
 
-console.log(hello()) // eslint-disable-line no-console
+  console.log(hello()) // eslint-disable-line no-console
+}
