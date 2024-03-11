88f606608030069c7fd0159ca721309aa712c579
rochdev <roch.devost@gmail.com>
2024-03-11 17:39:21 -0400

    disable old node versions with matrix instead of in test

--- test/neon/test.js
+++ test/neon/test.js
@@ -1,8 +1,5 @@
 'use strict'
 
-// Neon only supports Node >=14
-if (process.versions.node.split('.')[0] >= '14') {
-  const { hello } = require('node-gyp-build')(__dirname)
+const { hello } = require('node-gyp-build')(__dirname)
 
-  console.log(hello()) // eslint-disable-line no-console
-}
+console.log(hello()) // eslint-disable-line no-console
