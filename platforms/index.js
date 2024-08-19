58bed3b052a0dbdad69e940104a3b6f65f6035aa
rochdev <roch.devost@gmail.com>
2024-08-19 16:38:07 -0400

    fix interdependencies when skipping platforms

---
+++ platforms/index.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const { parseArgs } = require('node:util')
+
+const systems = ['darwin', 'linux', 'linuxglibc', 'linuxmusl', 'win32']
+const architectures = ['arm', 'arm64', 'ia32', 'x64']
+const platforms = systems.flatMap(s => architectures.map(a => `${s}-${a}`))
+
+const { values } = parseArgs({
+  args: process.argv.slice(2),
+  options: {
+    skip: { type: 'string', default: '' },
+    only: { type: 'string', default: '' }
+  }
+})
+
+function match (platform, filter) {
+  filter = filter.replace('windows', 'win32').replace('macos', 'darwin')
+
+  return filter.find(f => platform === f || platform.startsWith(filter) || platform.endsWith(filter))
+}
+
+function output (platforms) {
+  // eslint-disable-next-line no-console
+  console.log(`platforms=${JSON.stringify(platforms)}`)
+}
+
+if (values.only) {
+  const only = values.only.split(',')
+
+  output(platforms.filter(p => match(p, only)))
+} else if (values.skip) {
+  const skip = values.skip.split(',')
+
+  output(platforms.filter(p => !match(p, skip)))
+} else {
+  output(platforms)
+}
