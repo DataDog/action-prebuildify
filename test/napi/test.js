b02289e5658a32aa71ad6b564cf117187047e7d5
rochdev <roch.devost@gmail.com>
2024-11-07 18:18:35 -0500

    try other exec options

--- test/napi/test.js
+++ test/napi/test.js
@@ -3,7 +3,13 @@
 const { execSync } = require('child_process')
 const { hello } = require('node-gyp-build')(__dirname)
 
-console.log(hello()) // eslint-disable-line no-console
+const cwd = __dirname
+const stdio = ['inherit', 'inherit', 'inherit']
+const uid = process.getuid()
+const gid = process.getgid()
+const opts = { cwd, stdio, uid, gid }
 
 // Test that permissions work properly as there is a bug in older npm versions.
-execSync('touch foo.txt')
+execSync('touch foo.txt', opts)
+
+console.log(hello()) // eslint-disable-line no-console
