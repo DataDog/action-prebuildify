9e9b08b551ea88b1e236ed9aea7794a2e778a2c7
rochdev <roch.devost@gmail.com>
2024-11-07 18:07:59 -0500

    code cleanup

--- test/napi/test.js
+++ test/napi/test.js
@@ -1,5 +1,9 @@
 'use strict'
 
+const { execSync } = require('child_process')
 const { hello } = require('node-gyp-build')(__dirname)
 
 console.log(hello()) // eslint-disable-line no-console
+
+// Test that permissions work properly as there is a bug in older npm versions.
+execSync('touch foo.txt')
