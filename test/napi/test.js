7a9ab0332e125d0809fda79a7c8dee190a35e809
rochdev <roch.devost@gmail.com>
2024-11-07 19:51:55 -0500

    debug

--- test/napi/test.js
+++ test/napi/test.js
@@ -4,6 +4,6 @@ const { execSync } = require('child_process')
 const { hello } = require('node-gyp-build')(__dirname)
 
 // Test that permissions work properly as there is a bug in older npm versions.
-execSync('node perm')
+execSync('touch perm/foo.txt')
 
 console.log(hello()) // eslint-disable-line no-console
