605c34bb1d6f002614f11b7678279a6be3e6c923
rochdev <roch.devost@gmail.com>
2024-11-08 07:57:52 -0500

    remove permission test

--- test/napi/perm/index.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-const { execSync } = require('child_process')
-
-execSync('touch test/foo.txt')
--- test/napi/test.js
+++ test/napi/test.js
@@ -1,9 +1,5 @@
 'use strict'
 
-const { execSync } = require('child_process')
 const { hello } = require('node-gyp-build')(__dirname)
-
-// Test that permissions work properly as there is a bug in older npm versions.
-execSync('touch perm/foo.txt')
 
 console.log(hello()) // eslint-disable-line no-console
