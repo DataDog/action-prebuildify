72bcb4e98dc8618383791007088449364db99f63
rochdev <roch.devost@gmail.com>
2024-11-07 19:44:40 -0500

    debug

--- test/napi/perm/index.js
+++ test/napi/perm/index.js
@@ -2,8 +2,4 @@
 
 const { execSync } = require('child_process')
 
-const cwd = __dirname
-const stdio = ['inherit', 'inherit', 'inherit']
-const opts = { cwd, stdio }
-
-execSync('touch foo.txt', opts)
+execSync('touch test/foo.txt')
