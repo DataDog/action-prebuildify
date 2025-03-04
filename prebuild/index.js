688fb30114e25b3d782da34df1eb84a73aeb729a
rochdev <roch.devost@gmail.com>
2025-03-04 10:54:14 -0500

    fix missing rustup toolchain install command from breaking change

--- prebuild/index.js
+++ prebuild/index.js
@@ -144,4 +144,5 @@ function installRust () {
     "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s",
     `-y --verbose --no-update-default-toolchain --default-host ${target}`
   ].join(' -- '), { cwd, stdio, shell })
+  execSync('rustup toolchain install', { cwd, stdio, shell })
 }
