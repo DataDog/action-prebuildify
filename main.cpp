5d2976a1aaaa92f6fc4eb377acae898760762306
rochdev <roch.devost@gmail.com>
2025-05-09 18:53:46 -0400

    remove nan usage

--- main.cpp
+++ main.cpp
@@ -1,22 +1,21 @@
-#include <nan.h>
-#include <v8.h>
+#include <node.h>
 
-namespace datadog {
-  namespace {
-    static void test(const v8::FunctionCallbackInfo<v8::Value>& info) {
-      info.GetReturnValue().Set(Nan::New("success").ToLocalChecked());
-    }
-  }
+using v8::FunctionCallbackInfo;
+using v8::Isolate;
+using v8::Local;
+using v8::NewStringType;
+using v8::Object;
+using v8::String;
+using v8::Value;
 
-  NODE_MODULE_INIT() {
-    v8::Isolate* isolate = context->GetIsolate();
-    void* data = nullptr;
-    v8::Local<v8::External> external = v8::External::New(isolate, data);
-
-    exports->Set(
-      context,
-      Nan::New("test").ToLocalChecked(),
-      v8::FunctionTemplate::New(isolate, test, external)->GetFunction(context).ToLocalChecked()
-    ).FromJust();
-  }
+void Method(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  args.GetReturnValue().Set(String::NewFromUtf8(
+    isolate, "world", NewStringType::kNormal).ToLocalChecked());
 }
+
+void Initialize(Local<Object> exports) {
+  NODE_SET_METHOD(exports, "hello", Method);
+}
+
+NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
